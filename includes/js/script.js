7952246627f812880b9a4a12a7d86d433e006407
Fredrik Johansen <fredrik1206@gmail.com>
2020-12-02 08:28:41 +0100

    Added code highlight

--- includes/js/script.js
+++ includes/js/script.js
@@ -9,8 +9,7 @@ $(document).ready(function () {
 		height: 500,
 		disableGrammar: false,
 		styleTags: [
-		//	{ title: 'Blockquote', tag: 'blockquote', className: 'blockquote', value: 'blockquote' },
-		'p', 'blockquote', 'pre', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
+			'p', 'blockquote', 'pre', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
 		],
 	});
 });
@@ -21,16 +20,28 @@ $("#summernote").on("summernote.change", function (we, contents, $editable) {
 	//console.log($("#summernote").summernote("code"));
 	
 	addBlockQuote();
+	addCodeBlock();
 });
 
 
 function addBlockQuote() {
 	var blogpost = document.getElementById("editorPreview");
 	var blockquote = blogpost.getElementsByTagName("blockquote");
-	console.log(blockquote);
+	//console.log(blockquote);
 	for (var i = 0; i < blockquote.length; i++) {
 		for (let j = 0; j < blockquoteClasses.length; j++) {
 			blockquote[i].classList.add(blockquoteClasses[j]);
 		}
+	}
+}
+
+
+function addCodeBlock() {
+	var blogpost = document.getElementById("editorPreview");
+	var codeblock = blogpost.getElementsByTagName("pre");
+	var element = document.createElement("code");
+	for (let i = 0; i < codeblock.length; i++) {
+		console.log("Added Code!");
+		codeblock[i].appendChild(element);  
 	}
 }
